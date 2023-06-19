791ef1e8562c6c60bf7d7b108fc5a1c39cbed865
Alexander Ragland <adraglan@ucsc.edu>
2023-06-18 18:41:31 -0700

    username prompt added

--- pizza/scripts/main.js
+++ pizza/scripts/main.js
@@ -13,3 +13,37 @@ myImage.onclick = () =>
         myImage.setAttribute("src", "images/pizza.jpg");
     }
 }
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName()
+{
+    const myName = prompt("Please enter your name");
+
+    if (!myName)
+    {
+        setUserName();
+    }
+    else
+    {
+        localStorage.setItem("name", myName);
+        myHeading.textContent = `Beer and pizza are cool, ${myName}`;
+    }
+    
+}
+
+if (!localStorage.getItem("name"))
+{
+    setUserName();
+}
+else
+{
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Beer and pizza are cool, ${storedName}`;
+}
+
+myButton.onclick = () =>
+{
+    setUserName();
+}
